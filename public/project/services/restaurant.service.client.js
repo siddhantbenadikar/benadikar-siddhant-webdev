39b92efada5e44181fa057bb1d08dcf1ff16aa17
siddhantbenadikar <siddhantn@hotmail.com>
2017-08-04 18:56:42 -0400

    text input working

--- public/project/services/restaurant.service.client.js
+++ public/project/services/restaurant.service.client.js
@@ -12,17 +12,17 @@
 
         function findLocation(query) {
             var url = "https://developers.zomato.com/api/v2.1/locations?query=" + query;
-            return  $http.get(url,{"user-key" : "ab0bf5a65624e10925ce1720007a332"});
+            return  $http.get(url, {"user_key" : "ab0bf5a65624e10925ce1720007a332", "Accept" : "application/json"});
         }
         
         function searchRestaurantsByLocation(entityId) {
             var url = "https://developers.zomato.com/api/v2.1/search?entity_id="+entityId.toString()+"&entity_type=city&sort=rating"
-            return $http.get(url,{"user-key" : "ab0bf5a65624e10925ce1720007a332"});
+            return  $http.get(url, {"user_key" : "ab0bf5a65624e10925ce1720007a332", "Accept" : "application/json"});
         }
 
         function searchRestaurantById(rid) {
             var url = "https://developers.zomato.com/api/v2.1/restaurant?res_id="+rid;
-            return $http.get(url,{"user-key" : "ab0bf5a65624e10925ce1720007a332"});
+            return  $http.get(url, {"user_key" : "ab0bf5a65624e10925ce1720007a332", "Accept" : "application/json"});
         }
     }
 })();
